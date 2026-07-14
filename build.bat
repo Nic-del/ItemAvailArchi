7e5e644a5f3ff5085fcf9f7c7ad6bdb148a83f17
Nic-del <104897065+Nic-del@users.noreply.github.com>
2026-07-14 22:23:12 +0200

    Untrack build.bat and add it to .gitignore

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo =======================================
-echo Starting compilation with PyInstaller...
-echo =======================================
-pyinstaller AP_Mini_Tracker.spec
-echo =======================================
-echo Compilation finished!
-echo =======================================
-pause
